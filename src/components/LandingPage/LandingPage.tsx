ba39cec5f9353bf485af9ed912f525d90c1e40a2
PiotrOrebski <piotr.obrebski94@gmail.com>
2020-11-14 13:39:44 +0100

    Add Landing page return structure.

--- src/components/LandingPage/LandingPage.tsx
+++ src/components/LandingPage/LandingPage.tsx
@@ -1,9 +1,12 @@
+import './LandingPage.css';
+
 import React, { ReactElement, useEffect } from 'react';
 import { connect } from 'react-redux';
 
-import { GAMES_END_POINT, USER_END_POINT } from '../api-end-points';
-import { getDataFromServer } from '../api-helper';
-import { setAllGames, setUser } from '../redux/actions';
+import { GAMES_END_POINT, USER_END_POINT } from '../../api-end-points';
+import { getDataFromServer } from '../../api-helper';
+import { setAllGames, setUser } from '../../redux/actions';
+import { GamesList } from '../GamesList/GamesList';
 
 const LandingPage = (
   props: {
@@ -25,21 +28,20 @@ const LandingPage = (
     .then( response => {
       props.setAllGames(response)
     })
+
+    // putScoreOnServer(SCORE_END_POINT, {
+    //   "user_id": "f7bbbdd9-5a55-4a32-b53e-b5b74b4d24b4",
+    //   "score": '48',
+    //   "game_id": "c77f35e3-d41c-446c-af63-80f430a962d0"
+    // })
+    // .then( response => console.log(response));
   }, []);
 
   return (
-  <div className="todo-app">
+  <div className="landing-page">
     <h1>Hello {props.user.name}</h1>
     <div>What would you like to play today?</div>
-    {props.allGames.gamesArray.map(
-      (game, id)=>{
-        return(
-          <div key={id}>
-            {game.name}
-          </div>
-        )
-      }
-    )}
+    <GamesList allGames={props.allGames}/>
   </div>
   )
 };
