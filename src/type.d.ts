4ff23f682dcddd85f86091cae4df943d2df7fa67
PiotrOrebski <piotr.obrebski94@gmail.com>
2020-11-16 10:27:57 +0100

    Add basic launchedGame interface definition.

--- src/type.d.ts
+++ src/type.d.ts
@@ -4,10 +4,15 @@ interface User {
   email: string
 }
 
-interface Game {
+interface IGame {
   id: string,
   name:string,
   intro_text:string,
   farewell_text:string,
   time: number
+}
+
+interface ILaunchedGame {
+  volume: boolean,
+  score: number
 }
