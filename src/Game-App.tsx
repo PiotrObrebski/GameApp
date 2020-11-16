fe482cbbf3b1e2d0c40f3e4842255bc3de91ee9a
PiotrOrebski <piotr.obrebski94@gmail.com>
2020-11-16 10:32:00 +0100

    Add new route to the app containing Game component.

--- src/Game-App.tsx
+++ src/Game-App.tsx
@@ -3,19 +3,23 @@ import './Game-App.css';
 import React, { FC, ReactElement } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
+import Game from './components/Game/Game';
 import GamePage from './components/GamePage/GamePage';
 import LandingPage from './components/LandingPage/LandingPage';
 
 const GameApp: FC = (): ReactElement => {
   return (
     
-    <div className="wrapper game-app">
+    <div className="game-app">
+      <div className="wrapper">
       <Router>
         <Switch>
           <Route exact path="/" component={LandingPage}/>
-          <Route path="/:name" component={GamePage}/>
+          <Route exact path="/:name" component={GamePage}/>
+          <Route path="/:name/play" component={Game}/>
         </Switch>
       </Router>
+      </div>
     </div>
   );
 }
