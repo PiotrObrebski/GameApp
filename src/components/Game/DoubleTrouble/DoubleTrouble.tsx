2eb98e871a56162e1138d42ba5b29d0a076aecdf
PiotrOrebski <piotr.obrebski94@gmail.com>
2020-11-17 07:59:06 +0100

    Add double trouble handling of startGame.

--- src/components/Game/DoubleTrouble/DoubleTrouble.tsx
+++ src/components/Game/DoubleTrouble/DoubleTrouble.tsx
@@ -5,30 +5,41 @@ import { ReactElement } from 'react';
 import { connect } from 'react-redux';
 import { Link, RouteComponentProps } from 'react-router-dom';
 
+import { startGame } from '../../../redux/actions';
 import ScoreControl from '../ScoreControl/ScoreControl';
 import SoundControl from '../SoundControl/SoundControl';
 import TimeControl from '../TimeControl/TimeControl';
 
 interface IDoubleTroubleProps extends RouteComponentProps<{ name: string}> {
   currentGame: IGame;
+  launchedGame: ILaunchedGame;
+  startGame: () => void;
 }
 const DoubleTrouble = (props: IDoubleTroubleProps): ReactElement => {
+  
   return (
     <div className="game-double-trouble">
       
       <div className="game-double-trouble-body">
-        <div className="play-wrapper">
-          <div className="play-button">
-            <i className="fas fa-play"></i>
+        {!props.launchedGame.gameActive ? 
+          <div className="prepare-for-game">
+            <div className="play-wrapper">
+              <div onClick={props.startGame} className="play-button">
+                <i className="fas fa-play"></i>
+              </div>
+            </div>
+            <div className="start-text">Start the game</div>
+            <Link
+              className="back-to-desc"
+              to={`/${props.currentGame.name}`}
+            >
+              Instructions
+            </Link>
+          </div> : 
+          <div className="launched-game">
+            Go!
           </div>
-        </div>
-        <div className="start-text">Start the game</div>
-        <Link
-          className="back-to-desc"
-          to={`/${props.currentGame.name}`}
-        >
-          Instructions
-        </Link>
+        }
       </div>
       
       <div className="game-double-trouble-aside">
@@ -51,13 +62,16 @@ const DoubleTrouble = (props: IDoubleTroubleProps): ReactElement => {
 const mapStateToProps = (
   state: {
     currentGame: IGame;
+    launchedGame: ILaunchedGame;
   }
 ) => {
-  const { currentGame } = state;
-  return { currentGame };
+  const { currentGame, launchedGame } = state;
+  return { currentGame, launchedGame };
 }
 
 export default connect(
   mapStateToProps, 
-  {}
+  {
+    startGame
+  }
 )(DoubleTrouble);
