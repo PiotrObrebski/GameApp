bbea045f6cbc7bce3a0c33771f78aa404b564814
PiotrOrebski <piotr.obrebski94@gmail.com>
2020-11-22 14:04:20 +0100

    Add snapshot test of Game-App component.

--- src/Game-App.test.tsx
+++ src/Game-App.test.tsx
@@ -1,20 +1,30 @@
 import { shallow, ShallowWrapper } from 'enzyme';
 import React from 'react';
+import { Provider } from 'react-redux';
+import renderer from 'react-test-renderer';
 
 import GameApp from './Game-App';
+import store from './redux/store';
 
-describe('GameApp', () => {
-  let component: ShallowWrapper<{any: any}>
+describe("GameApp", () => {
+  let component: ShallowWrapper<{ any: any }>;
 
   beforeEach(() => {
-    component = shallow(<GameApp/>)
-  })
-  test('Should mount', () => {
-    console.log(component.debug());
-    expect(component.length).toBe(1)
-  })
-  // test('should match snapshot', () => {
-  //   expect(component).toMatchSnapshot();
-  // }
-  // )
-})
+    component = shallow(<GameApp />);
+  });
+
+  test("Should mount", () => {
+    expect(component.length).toBe(1);
+  });
+
+  test("should match snapshot", () => {
+    const GameAppRender = renderer
+      .create(
+        <Provider store={store}>
+          <GameApp />
+        </Provider>
+      )
+      .toJSON();
+    expect(GameAppRender).toMatchSnapshot();
+  });
+});
