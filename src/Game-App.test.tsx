378d041a30b30129aaee7752ccf81c80e9760f4a
PiotrOrebski <piotr.obrebski94@gmail.com>
2020-11-22 12:38:39 +0100

    Modify first test to match implementation and typescript

--- src/Game-App.test.tsx
+++ src/Game-App.test.tsx
@@ -1,15 +1,20 @@
-import { shallow } from 'enzyme';
+import { shallow, ShallowWrapper } from 'enzyme';
 import React from 'react';
 
 import GameApp from './Game-App';
 
-describe('<App />', () => {
-  let component
+describe('GameApp', () => {
+  let component: ShallowWrapper<{any: any}>
 
   beforeEach(() => {
     component = shallow(<GameApp/>)
   })
-  test('It should mount', () => {
+  test('Should mount', () => {
+    console.log(component.debug());
     expect(component.length).toBe(1)
   })
+  // test('should match snapshot', () => {
+  //   expect(component).toMatchSnapshot();
+  // }
+  // )
 })
